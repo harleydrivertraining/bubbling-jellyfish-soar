e00ee5243189324537c370311bd0bca2a22bbd94
[dyad] <harleydrivertraining@gmail.com>
2025-10-05 21:49:31 +0100

    [dyad] Fixed 404 error for Settings page by adding its route to App.tsx. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
 import Settings from "./pages/Settings";
 import Schedule from "./pages/Schedule";
-import Lessons from "./pages/Lessons"; // Import new page
-import LessonNotes from "./pages/LessonNotes"; // Import new page
-import StudentTargets from "./pages/StudentTargets"; // Import new page
-import Progress from "./pages/Progress"; // Import new page
-import DrivingTests from "./pages/DrivingTests"; // Import new page
-import PrePaidHours from "./pages/PrePaidHours"; // Import new page
-import Resources from "./pages/Resources"; // Import new page
+import Lessons from "./pages/Lessons";
+import LessonNotes from "./pages/LessonNotes";
+import StudentTargets from "./pages/StudentTargets";
+import Progress from "./pages/Progress";
+import DrivingTests from "./pages/DrivingTests";
+import PrePaidHours from "./pages/PrePaidHours";
+import Resources from "./pages/Resources";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import { SessionContextProvider } from "./components/auth/SessionContextProvider";
@@ -34,13 +34,14 @@ const App = () => (
               <Route index element={<Dashboard />} />
               <Route path="students" element={<Students />} />
               <Route path="schedule" element={<Schedule />} />
-              <Route path="lessons" element={<Lessons />} /> {/* New route */}
-              <Route path="lesson-notes" element={<LessonNotes />} /> {/* New route */}
-              <Route path="student-targets" element={<StudentTargets />} /> {/* New route */}
-              <Route path="progress" element={<Progress />} /> {/* New route */}
-              <Route path="driving-tests" element={<DrivingTests />} /> {/* New route */}
-              <Route path="pre-paid-hours" element={<PrePaidHours />} /> {/* New route */}
-              <Route path="resources" element={<Resources />} /> {/* New route */}
+              <Route path="lessons" element={<Lessons />} />
+              <Route path="lesson-notes" element={<LessonNotes />} />
+              <Route path="student-targets" element={<StudentTargets />} />
+              <Route path="progress" element={<Progress />} />
+              <Route path="driving-tests" element={<DrivingTests />} />
+              <Route path="pre-paid-hours" element={<PrePaidHours />} />
+              <Route path="resources" element={<Resources />} />
+              <Route path="settings" element={<Settings />} /> {/* Added missing route for Settings */}
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Route>
