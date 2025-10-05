76041128c1063b2e58e4eabfc85c94159ee4abe1
[dyad] <harleydrivertraining@gmail.com>
2025-10-05 21:53:17 +0100

    [dyad] Fixed the Lessons page by importing the missing FormLabel component. - wrote 1 file(s)

--- src/pages/Lessons.tsx
+++ src/pages/Lessons.tsx
@@ -15,6 +15,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { FormLabel } from "@/components/ui/form"; // <--- Added this import
 
 interface Booking {
   id: string;
