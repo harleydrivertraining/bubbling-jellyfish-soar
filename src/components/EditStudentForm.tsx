407845d0513b8eb7f423426289b7e9ea360bd535
[dyad] <harleydrivertraining@gmail.com>
2025-10-07 10:24:12 +0100

    [dyad] Fixed syntax error in EditStudentForm by removing an extra div tag - wrote 1 file(s)

--- src/components/EditStudentForm.tsx
+++ src/components/EditStudentForm.tsx
@@ -487,7 +487,6 @@ const EditStudentForm: React.FC<EditStudentFormProps> = ({ studentId, onStudentU
             </AlertDialogContent>
           </AlertDialog>
         </div>
-        </div>
       </form>
     </Form>
   );
