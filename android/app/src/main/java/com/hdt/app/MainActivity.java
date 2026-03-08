955578b6306a7663f647b89bc3e44ba628142a7d
[dyad] <harleydrivertraining@gmail.com>
2026-03-08 18:22:17 +0000

    [dyad] Implemented Android status bar fix using Window Insets and CSS safe area variables. - wrote 3 file(s)

--- android/app/src/main/java/com/hdt/app/MainActivity.java
+++ android/app/src/main/java/com/hdt/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.hdt.app;
 
+import android.os.Bundle;
+import android.view.View;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Force the content to respect the status bar by adding padding to the root view
+        // This is necessary for Android 15 (API 35) which is edge-to-edge by default
+        View rootView = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
+            int top = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+            v.setPadding(0, top, 0, 0);
+            return insets;
+        });
+    }
+}
