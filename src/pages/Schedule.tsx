3bf45605083952a16816bffeefae62a28e70067d
[dyad] <harleydrivertraining@gmail.com>
2026-03-18 00:17:23 +0000

    [dyad] Fixed 'cn is not defined' error in Schedule page. - wrote 1 file(s)

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -14,6 +14,7 @@ import { Event as BigCalendarEvent } from 'react-big-calendar';
 import { useIsMobile } from "@/hooks/use-mobile";
 import CalendarLegend from "@/components/CalendarLegend";
 import { Link } from "react-router-dom";
+import { cn } from "@/lib/utils";
 
 const Schedule: React.FC = () => {
   const { user, isLoading: isSessionLoading, initialBookings, isLoadingInitialBookings } = useSession();
