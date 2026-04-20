a31f08e5bd058540654bed6e2cdb0cc6e2032679
[dyad] <harleydrivertraining@gmail.com>
2026-04-20 23:35:34 +0100

    [dyad] Removed the external registration link from the login screen. - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -143,15 +143,6 @@ const Login: React.FC = () => {
             </div>
           </TabsContent>
         </Tabs>
-
-        <div className="text-center pt-4">
-          <p className="text-sm text-muted-foreground">
-            Don't have an account?{" "}
-            <a href="https://drivinginstructorapp.uk" target="_blank" rel="noopener noreferrer" className="text-primary font-bold hover:underline">
-              Visit drivinginstructorapp.uk to get started
-            </a>
-          </p>
-        </div>
       </div>
     </div>
   );
