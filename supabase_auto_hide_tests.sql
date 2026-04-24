e05fd13786ead4d1636c631873a106f6eb132ec2
[dyad] <harleydrivertraining@gmail.com>
2026-04-24 01:05:35 +0100

    [dyad] Fixed schema cache error by providing SQL to add missing column. - wrote 1 file(s)

--- supabase_auto_hide_tests.sql
+++ supabase_auto_hide_tests.sql
@@ -1,1 +1,5 @@
+-- Add the missing column
 ALTER TABLE profiles ADD COLUMN IF NOT EXISTS auto_hide_test_dates BOOLEAN DEFAULT TRUE;
+
+-- Notify PostgREST to reload the schema cache
+NOTIFY pgrst, 'reload schema';
